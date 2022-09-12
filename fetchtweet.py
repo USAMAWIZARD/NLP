bec3749973c39de9c78497e4a0a76806dcf9265c
USAMA <usamawizard@gmail.com>
2022-09-12 07:36:01 -0700

    Update fetchtweet.py

--- fetchtweet.py
+++ fetchtweet.py
@@ -1,20 +1,20 @@
-    tweetlist = []
-    entitylist = ['technology','mobile','tablets']
-    listindex = 0
-    import tweepy
+tweetlist = []
+entitylist = ['technology','mobile','tablets']
+listindex = 0
+import tweepy
 
-    auth = tweepy.OAuthHandler("BB8uhuyrMzHesokpE7RqcYemc", "7YhL2UOSVoUyquuImiT1uOag0gS5HR3mrt7mmXrNZbwNHOVklF")
-    auth.set_access_token("2330384770-kx4jr7mfUsCJod2MNuff3VzCh6tbGCvdymwp3ND",
-                          "IMEYoDK2gZITSztqKQvHohTMOPkmEjsA0w6AOoECIdCqu")
-    api = tweepy.API(auth)
-    count = 0
+auth = tweepy.OAuthHandler("BB8uhuyrMzHesokpE7RqcYemc", "7YhL2UOSVoUyquuImiT1uOag0gS5HR3mrt7mmXrNZbwNHOVklF")
+auth.set_access_token("2330384770-kx4jr7mfUsCJod2MNuff3VzCh6tbGCvdymwp3ND",
+                      "IMEYoDK2gZITSztqKQvHohTMOPkmEjsA0w6AOoECIdCqu")
+api = tweepy.API(auth)
+count = 0
 
-    for i in range(20):
-        for tweet in api.search(entitylist[listindex]):
-            tweetlist.append(tweet.text)
-        count += 1
-        if count == 1000:
-            listindex += 1
-            count = 0
+for i in range(20):
+    for tweet in api.search(entitylist[listindex]):
+        tweetlist.append(tweet.text)
+    count += 1
+    if count == 1000:
+        listindex += 1
+        count = 0
 
-    print(tweetlist)
+print(tweetlist)
